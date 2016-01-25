8172491899f99d5d5a89a7b5c4b9652ed00864a0
Dennis Castleberry <dcastl2@tigers.lsu.edu>
2016-01-25 11:54:56 -0600

    Added more starter code/files

---
+++ assg/prog2a/src/polynomial.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+
+/**
+ * This is an O(n^2) algorithm given that the implementation of pow is linear
+ * in n.  To make it more efficient, apply Horner's algorithm for the
+ * evaluation of a polynomial.
+ */
+int polynomial(int p[], int n, int x) {
+  int result = 0;
+  for (int i=0; i<n; i++)
+    result += p[i] * pow(x, i);
+  return result;
+}
+
+int main() {
+  int a[4] = {2, -2, 3, 4};
+  std::cout << polynomial(a, 4, 2) << std::endl;
+}
